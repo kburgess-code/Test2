83a6d844657bb6b0348f6163a52011166c3fda95
kburgess-code <kburgess@mcpsva.org>
2026-09-13 16:22:42 -0400

    Update MadLibs.py

--- MadLibs.py
+++ MadLibs.py
@@ -13,11 +13,15 @@ w1 = input("Name: ")
 w2 = input("Plural noun: ")
 w3 = input("Food item: ")
 
+w4 = input("Weather condition: ")
+w5 = input("Number: ")
+w6 = input("Length of time: ")
 
-print(f"{w1} went to {w2} {w3} place today!")
-print(f"{w2} went to {w3} {w1} place today.")
 
+print(f"\n{w1} went to {w2} {w3} place today!")
+#print(f"{w2} went to {w3} {w1} place today.")
 
+print(f"It was {w4} outside. The weather lasted for {w5} {w6}.")
 
 
 
